00508e5b919d8b4a5665d7bce6a5cd6f6579c248
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-05-08 22:23:08 +0530

    basic implementation done

--- app/src/main/java/com/amitupadhyay/displaycontacts/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/displaycontacts/MainActivity.java
@@ -2,10 +2,12 @@ package com.amitupadhyay.displaycontacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.amitupadhyay.displaycontacts.utils.Contact;
 import com.amitupadhyay.displaycontacts.utils.ContactFetcher;
+import com.amitupadhyay.displaycontacts.utils.ContactsRecyclerAdapter;
 
 import java.util.ArrayList;
 
@@ -21,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         contactsRV = (RecyclerView) findViewById(R.id.contactsRecyclerView);
         contactsList = new ContactFetcher(this).fetchAll();
+        ContactsRecyclerAdapter recyclerAdapter = new ContactsRecyclerAdapter(contactsList, MainActivity.this, R.layout.adapter_contact_item);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        contactsRV.setLayoutManager(llm);
+        contactsRV.setAdapter(recyclerAdapter);
     }
 }
