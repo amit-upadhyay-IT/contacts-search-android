2441ac238aca31503753d74039b042a553b5fdcb
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-05-08 21:47:16 +0530

    first commit

---
+++ app/src/main/java/com/amitupadhyay/displaycontacts/utils/ContactEmail.java
@@ -0,0 +1,15 @@
+package com.amitupadhyay.displaycontacts.utils;
+
+/**
+ * Created by aupadhyay on 5/8/17.
+ */
+
+public class ContactEmail {
+    public String address;
+    public String type;
+
+    public ContactEmail(String address, String type) {
+        this.address = address;
+        this.type = type;
+    }
+}
---
+++ app/src/main/java/com/amitupadhyay/displaycontacts/utils/ContactPhone.java
@@ -0,0 +1,16 @@
+package com.amitupadhyay.displaycontacts.utils;
+
+/**
+ * Created by aupadhyay on 5/8/17.
+ */
+
+public class ContactPhone {
+    public String number;
+    public String type;
+
+    public ContactPhone(String number, String type) {
+        this.number = number;
+        this.type = type;
+    }
+}
+
