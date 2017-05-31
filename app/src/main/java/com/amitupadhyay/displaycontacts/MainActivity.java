d48e9b01a54155669aae1a565b3c2fcc45f929b7
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-05-31 19:42:10 +0530

    asfdklsdf kasdf

--- app/src/main/java/com/amitupadhyay/displaycontacts/MainActivity.java
+++ app/src/main/java/com/amitupadhyay/displaycontacts/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.amitupadhyay.displaycontacts.utils.Contact;
 import com.amitupadhyay.displaycontacts.utils.ContactFetcher;
@@ -78,4 +80,24 @@ public class MainActivity extends AppCompatActivity {
             // permissions this app might request
         }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.menu_search)
+        {
+            //startActivity(new Intent(MainActivity.this, SearchActivity.class));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_corner_menu, menu);
+        return true;
+    }
+
 }
